067b8a67c01d70a5d24e7e4d0c779e16e3630dee
chatrli <chatrli@proton.me>
2023-11-21 23:51:56 +0100

    add CRUD for Book Controller

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View|\Illuminate\Foundation\Application|Factory|Application
+    {
+        $books = Book::all();
+        return view('books.index', compact('books'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View|\Illuminate\Foundation\Application|Factory|Application
+    {
+        return view('books.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $book = new Book;
+        $book->fill($request->only('author_id', 'genre_id', 'title', 'description', 'isbn', 'published_year'));
+        $book->save();
+        return redirect()->route('books.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): View|\Illuminate\Foundation\Application|Factory|Application
+    {
+        $book = Book::findOrFail($id);
+        return view('books.show', compact('book'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id): View|\Illuminate\Foundation\Application|Factory|Application
+    {
+        $book = Book::findOrFail($id);
+        return view('books.edit', compact('book'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): RedirectResponse
+    {
+        $book = Book::findOrFail($id);
+        $book->fill($request->only('author_id', 'genre_id', 'title', 'description', 'isbn', 'published_year'));
+        $book->save();
+        return redirect()->route('books.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id): RedirectResponse
+    {
+        $book = Book::findOrFail($id);
+        $book->delete();
+        return redirect()->route('books.index');
+    }
+}
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * @method static findOrFail(string $id)
+ */
 class Book extends Model
 {
     use HasFactory;
