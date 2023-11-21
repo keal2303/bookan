42a0ca4809224d455ef535dca2313279f8071068
chatrli <chatrli@proton.me>
2023-11-21 23:20:19 +0100

    add CRUD for Genre Controller

---
+++ app/Http/Controllers/GenreController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Genre;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class GenreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
+    {
+        $genres = Genre::all();
+        return view('genres.index', compact('genres'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
+    {
+        return view('genres.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $genre = new Genre;
+        $genre->fill($request->only('name', 'description'));
+        $genre->save();
+        return redirect()->route('genres.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
+    {
+        $genre = Genre::findOrFail($id);
+        return view('genres.show', compact('genre'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
+    {
+        $genre = Genre::findOrFail($id);
+        return view('genres.edit', compact('genre'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): RedirectResponse
+    {
+        $genre = Genre::findOrFail($id);
+        $genre->fill($request->only('name', 'description'));
+        $genre->save();
+        return redirect()->route('genres.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id): RedirectResponse
+    {
+        $genre = Genre::findOrFail($id);
+        $genre->delete();
+        return redirect()->route('genres.index');
+    }
+}
