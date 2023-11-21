bb5074f50c0c680d42cef259a7bbaf9fe5c0000f
chatrli <chatrli@proton.me>
2023-11-21 23:35:32 +0100

    add CRUD for Author Controller

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
+    {
+        $authors = Author::all();
+        return view('authors.index', compact('authors'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
+    {
+        return view('authors.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $author = new Author;
+        $author->fill($request->only('genre_id', 'name', 'bio', 'birth_year', 'death_year', 'nationality', 'link', 'media'));
+        $author->save();
+        return redirect()->route('author.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
+    {
+        $author = Author::findOrFail($id);
+        return view('authors.show', compact('author'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
+    {
+        $author = Author::findOrFail($id);
+        return view('authors.edit', compact('author'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): RedirectResponse
+    {
+        $author = Author::findOrFail($id);
+        $author->fill($request->only('genre_id', 'name', 'bio', 'birth_year', 'death_year', 'nationality', 'link', 'media'));
+        $author->save();
+        return redirect()->route('authors.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id): RedirectResponse
+    {
+        $author = Author::findOrFail($id);
+        $author->delete();
+        return redirect()->route('authors.index');
+    }
+}
--- app/Models/Author.php
+++ app/Models/Author.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * @method static findOrFail(string $id)
+ */
 class Author extends Model
 {
     use HasFactory;
