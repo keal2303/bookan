90340ef8f7734c5cfc9b40bc2dc88849f26ac7bf
chatrli <chatrli@proton.me>
2023-11-21 22:42:38 +0100

    modify Book fields

--- database/migrations/2023_11_21_205039_create_books_table.php
+++ database/migrations/2023_11_21_205039_create_books_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('author_id');
-            $table->unsignedBigInteger('genre_id');
+            $table->unsignedBigInteger('genre_id')->nullable();
             $table->string('title');
             $table->string('description');
             $table->string('isbn')->nullable();
