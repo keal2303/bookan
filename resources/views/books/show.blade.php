3006eff407938c8b78270563e2a452aa3b8e34ba
chatrli <chatrli@proton.me>
2024-01-07 22:43:24 +0100

    add edit and delete button on show page for Book

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -23,6 +23,12 @@
                     x-on:click.prevent="$dispatch('open-modal', 'create-review')">
                     Add Review
                 </x-primary-button>
+                <x-primary-button><a href="{{ route('books.edit', $book->id) }}">Edit Book</a></x-primary-button>
+                <form action="{{ route('books.destroy', $book->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <x-danger-button type="submit" class="mt-2">Delete</x-danger-button>
+                </form>
             </div>
             <br>
             <p><strong>Review Count: </strong>{{ $book->calculateReviewCount() }}</p>
